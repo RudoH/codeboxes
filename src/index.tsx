0bf277fe53059619c62af5559c507f89c61580a9
RudoH <rudohengst@gmail.com>
2021-01-25 20:44:34 -0500

    feature: basic JSX transpiling of user input

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,36 @@
+import * as esbuild from 'esbuild-wasm'
 import { render } from 'react-dom'
-import { useState } from 'react'
-
+import { useState, useEffect, useRef } from 'react'
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = () => {
-    console.log(input)
-    setCode(input)
+  // initialize esbuild service and assign to ref
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+
+  }
+
+  useEffect(() => {
+    startService();
+  }, [])
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015'
+    });
+
+    setCode(result.code)
   }
 
   return  (
