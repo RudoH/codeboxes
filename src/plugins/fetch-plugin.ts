f82d892df22b9bf83e8e442f22ab23ed67c79db9
RudoH <rudohengst@gmail.com>
2021-01-26 18:15:37 -0500

    Added support for CSS file imports

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,18 +10,46 @@ export const fetchPlugin = (inputCode: string): any => {
     return {
         name: 'fetch-plugin',
         setup(build: esbuild.PluginBuild) {
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                if (args.path === 'index.js') {
-                    return {
-                        loader: 'jsx',
-                        contents: inputCode,
-                    };
-                }
+            // loading of index file
+            build.onLoad({ filter: /(^index\.js$)/ }, () => {
+                return { loader: 'jsx', contents: inputCode };
+            });
 
+            // run on every request to check if cache contains result
+            // if not in cache - fall through to next onLoad
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
                 // if request is in cache - return immediately
                 const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
                 if (cachedResult) return cachedResult;
+            });
 
+            // loading of any css files
+            build.onLoad({ filter: /.css$/ }, async (args: any) => {
+                const { data, request } = await axios.get(args.path);
+
+                /*
+                / ESBuild can't output CSS files in the browser - so we 'inject' the CSS into a JS
+                / file to be read if the filetype is a .css file. This requires escaping the chars
+                / that would prematurely terminate the string.
+                */
+                const escaped = data.replace(/\n/g, '').replace(/"/g, '\\"').replace(/'/g, "\\'");
+                const contents = `
+                    const style = document.createElement('style');
+                    stlye.innerText = '${escaped}';
+                    document.head.appendChild(style);`;
+
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents,
+                    resolveDir: new URL('./', request.responseURL).pathname,
+                };
+
+                // add to cache before returning
+                await fileCache.setItem(args.path, result);
+                return result;
+            });
+
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
                 const { data, request } = await axios.get(args.path);
 
                 const result: esbuild.OnLoadResult = {
@@ -32,7 +60,6 @@ export const fetchPlugin = (inputCode: string): any => {
 
                 // add to cache before returning
                 await fileCache.setItem(args.path, result);
-
                 return result;
             });
         },
