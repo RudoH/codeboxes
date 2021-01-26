fbd0e0672b512581e0b8df00ed358abf1e42eeb4
RudoH <rudohengst@gmail.com>
2021-01-26 16:38:18 -0500

    Added linter/prettier with TS to repo

--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,54 @@
-import * as esbuild from 'esbuild-wasm'
-import { render } from 'react-dom'
-import { useState, useEffect, useRef } from 'react'
-import { unpkgPathPlugin} from './plugins/unpkg-path-plugin'
-import { fetchPlugin } from './plugins/fetch-plugin'
+import * as esbuild from 'esbuild-wasm';
+import { render } from 'react-dom';
+import { useState, useEffect, useRef } from 'react';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
-  const ref = useRef<any>();
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
 
-  // initialize esbuild service and assign to ref
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: '/esbuild.wasm'
-    });
-  }
+    // initialize esbuild service and assign to ref
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm',
+        });
+    };
 
-  useEffect(() => {
-    startService();
-  }, [])
+    useEffect(() => {
+        startService();
+    }, []);
 
-  const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
 
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [
-        unpkgPathPlugin(),
-        fetchPlugin(input)
-      ],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    })
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window',
+            },
+        });
 
-    setCode(result.outputFiles[0].text)
-  }
+        setCode(result.outputFiles[0].text);
+    };
 
-  return  (
-    <div>
-      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-    <pre>{code}</pre>
-    </div>
-  )
-}
+    return (
+        <div>
+            <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
+};
 
-render(
-  <App />, 
-  document.getElementById('root')
-)
+render(<App />, document.getElementById('root'));
