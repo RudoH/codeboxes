5934abb4f2fb17e5cf792e030b106aa90d59caa0
RudoH <rudohengst@gmail.com>
2021-01-25 20:25:39 -0500

    Initial input component

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import { render } from 'react-dom'
+import { useState } from 'react'
+
+
+const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input)
+    setCode(input)
+  }
+
+  return  (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+    <pre>{code}</pre>
+    </div>
+  )
+}
+
+render(
+  <App />, 
+  document.getElementById('root')
+)
