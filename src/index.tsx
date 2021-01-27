098c76f6dc08f554f4a3d825d0081814414c4847
RudoH <rudohengst@gmail.com>
2021-01-27 18:32:39 -0500

    Feature: Show compiled react components in iframe output

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,9 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
+    // const [code, setCode] = useState('');
 
     // initialize esbuild service and assign to ref
     const startService = async () => {
@@ -37,8 +38,23 @@ const App = () => {
             },
         });
 
-        setCode(result.outputFiles[0].text);
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     };
+
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener('message', (event) => {
+                        eval(event.data);
+                    }, false)
+                </script>
+            </body>
+        </html>
+    `;
 
     return (
         <div>
@@ -46,7 +62,7 @@ const App = () => {
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
+            <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
         </div>
     );
 };
