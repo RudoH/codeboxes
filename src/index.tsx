d819bd8dccfa2113bbcd2d72873804c1807d042a
RudoH <rudohengst@gmail.com>
2021-01-28 15:09:03 -0500

    Refactor: extracted CodeEditor/Preview into CodeCell component

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-
 import { render } from 'react-dom';
-import { useState } from 'react';
-
-import CodeEditor from './components/CodeEditor';
-import Preview from './components/Preview';
-import bundle from './bundler';
+import CodeCell from './components/CodeCell';
 
 const App = () => {
-    const [code, setCode] = useState('');
-    const [input, setInput] = useState('');
-
-    const onClick = async () => {
-        const output = await bundle(input);
-        setCode(output);
-    };
-
     return (
         <div>
-            <CodeEditor initialValue="hello" onChange={(value) => setInput(value)} />
-            <div>
-                <button onClick={onClick}>Submit</button>
-            </div>
-            <Preview code={code} />
+            <CodeCell />
         </div>
     );
 };
