cc7475d2069b361f78752b5c8b19d00961656b09
RudoH <rudohengst@gmail.com>
2021-01-30 14:53:22 -0500

    Feature: Markdown Editor (CodeEditor hidden temporarily)

---
+++ src/components/TextEditor.tsx
@@ -0,0 +1,38 @@
+import MDEditor from '@uiw/react-md-editor';
+import { useState, useEffect, useRef } from 'react';
+
+const TextEditor: React.FC = () => {
+    const editorRef = useRef<HTMLDivElement | null>(null);
+    const [editing, setEditing] = useState(false);
+
+    useEffect(() => {
+        const clickListener = (event: MouseEvent) => {
+            if (
+                editorRef.current &&
+                event.target &&
+                editorRef.current.contains(event.target as Node)
+            ) {
+                return console.log('inside editor');
+            }
+            console.log('outside editor');
+            setEditing(false);
+        };
+        document.addEventListener('click', clickListener, { capture: true });
+
+        return () => {
+            document.removeEventListener('click', clickListener, { capture: true });
+        };
+    }, []);
+
+    return editing ? (
+        <div ref={editorRef}>
+            <MDEditor />
+        </div>
+    ) : (
+        <div onClick={() => setEditing(true)}>
+            <MDEditor.Markdown source={'# Header'} />
+        </div>
+    );
+};
+
+export default TextEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import { render } from 'react-dom';
-import CodeCell from './components/CodeCell';
+// import CodeCell from './components/CodeCell';
+import TextEditor from './components/TextEditor';
 
 const App = () => {
     return (
         <div>
-            <CodeCell />
+            <TextEditor />
         </div>
     );
 };
