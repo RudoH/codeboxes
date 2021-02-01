af9e39669f92fb0ff83d68369a80c11257f7587a
RudoH <rudohengst@gmail.com>
2021-02-01 12:50:47 -0500

    Style: Add loading bar and smoother bundling refresh

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -5,6 +5,7 @@ import Resizable from './Resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/useActions';
 import { useSelector } from '../hooks/useTypedSelectors';
+import './CodeCell.css';
 
 interface CodeCellProps {
     cell: Cell;
@@ -38,7 +39,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }: CodeCellProps) => {
                             onChange={(value) => updateCell(cell.id, value)}
                         />
                     </Resizable>
-                    {bundle && <Preview code={bundle.code} errorStatus={bundle.err} />}
+                    <div className="progress-wrapper">
+                        {!bundle || bundle.loading ? (
+                            <div className="progress-cover">
+                                <progress className="progress is-small is-primary" max="100">
+                                    Loading
+                                </progress>
+                            </div>
+                        ) : (
+                            <Preview code={bundle.code} errorStatus={bundle.err} />
+                        )}
+                    </div>
                 </div>
             </Resizable>
         </div>
