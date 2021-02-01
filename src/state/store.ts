92ddb65d7348186c802426b4133ca880d6b45f5f
RudoH <rudohengst@gmail.com>
2021-02-01 07:00:46 -0500

    Remove: Delete temp test dispatches from store

--- src/state/store.ts
+++ src/state/store.ts
@@ -3,22 +3,3 @@ import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
-
-// Temp code for testing purposes
-import { ActionType } from './action-types';
-
-store.dispatch({
-    type: ActionType.INSERT_CELL_AFTER,
-    payload: {
-        id: null,
-        type: 'text',
-    },
-});
-
-store.dispatch({
-    type: ActionType.INSERT_CELL_AFTER,
-    payload: {
-        id: null,
-        type: 'code',
-    },
-});
